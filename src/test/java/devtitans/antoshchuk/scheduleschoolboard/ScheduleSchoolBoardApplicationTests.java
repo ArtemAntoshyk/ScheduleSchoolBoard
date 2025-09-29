8b7d0a264c6125060b7603ee2c70ae66ede2c87e
artem <artemantosuk4@gmail.com>
2025-09-29 21:33:04 +0300

    Initial commit

---
+++ src/test/java/devtitans/antoshchuk/scheduleschoolboard/ScheduleSchoolBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package devtitans.antoshchuk.scheduleschoolboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSchoolBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
